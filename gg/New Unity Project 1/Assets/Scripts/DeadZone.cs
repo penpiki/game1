fbbeec4e6c28cc381a7e5b43ecc18134d3da9e05
Ajanerd <2526623@gmail.com>
2016-10-11 10:42:31 +0900

    a

---
+++ gg/New Unity Project 1/Assets/Scripts/DeadZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DeadZone : MonoBehaviour {
+
+	void TriggerEnter (Collider hit)
+	{
+		if (hit.gameObject.CompareTag ("Player")) {
+
+			int sceneIndex = SceneManager.GetActiveScene ().buildIndex;
+
+			SceneManager.LoadScene (sceneIndex);
+		}
+	}
+
+}
